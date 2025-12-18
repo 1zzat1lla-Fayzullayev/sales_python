c740b8fcd23ea2a78c5f84d55653ee08dee01c02
1zzat1lla-Fayzullayev <izzatillaf495@gmail.com>
2025-12-18 12:13:40 +0500

    change

--- modules/products.py
+++ modules/products.py
@@ -1,7 +1,7 @@
 from database.db import read_data
 
 def list_products():
-    products = read_data("products.json")
+    products = read_data("products.csv")
     if not products:
         print("Mahsulotlar yo‘q")
         return
